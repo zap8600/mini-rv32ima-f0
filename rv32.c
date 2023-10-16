5b32122de79f067685b0338749c63a41c8ad0337
zap8600 <95487746+zap8600@users.noreply.github.com>
2023-10-16 09:37:20 -0500

    wip

--- rv32.c
+++ rv32.c
@@ -1,6 +1,15 @@
+#include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #include <furi.h>
+
+#include "default64mbdtc.h"
+
+uint32_t ram_amt = 64*1024*1024;
+int fail_on_all_faults = 0;
+File* file;
 
 static int64_t SimpleReadNumberInt( const char * number, int64_t defaultNumber );
 static uint64_t GetTimeMicroseconds();
@@ -14,3 +23,34 @@ static int32_t HandleOtherCSRRead( uint8_t * image, uint16_t csrno );
 static void MiniSleep();
 static int IsKBHit();
 static int ReadKBByte();
+
+static uint32_t store4(uint32_t ofs, uint32_t val);
+static uint16_t store2(uint32_t ofs, uint16_t val);
+static uint8_t store1(uint32_t ofs, uint8_t val);
+static uint32_t load4(uint32_t ofs);
+static uint16_t load2(uint32_t ofs);
+static uint8_t load1(uint32_t ofs);
+
+#define MINIRV32WARN( x... ) FURI_LOG_W( "mini-rv32ima", x );
+#define MINIRV32_DECORATE  static
+#define MINI_RV32_RAM_SIZE ram_amt
+#define MINIRV32_IMPLEMENTATION
+#define MINIRV32_POSTEXEC( pc, ir, retval ) { if( retval > 0 ) { if( fail_on_all_faults ) { FURI_LOG_E( "mini-rv32ima", "FAULT\n" ); return 3; } else retval = HandleException( ir, retval ); } }
+#define MINIRV32_HANDLE_MEM_STORE_CONTROL( addy, val ) if( HandleControlStore( addy, val ) ) return val;
+#define MINIRV32_HANDLE_MEM_LOAD_CONTROL( addy, rval ) rval = HandleControlLoad( addy );
+#define MINIRV32_OTHERCSR_WRITE( csrno, value ) HandleOtherCSRWrite( image, csrno, value );
+#define MINIRV32_OTHERCSR_READ( csrno, value ) value = HandleOtherCSRRead( image, csrno );
+
+#define MINIRV32_CUSTOM_MEMORY_BUS
+#define MINIRV32_STORE4( ofs, val ) store4(ofs, val)
+#define MINIRV32_STORE2( ofs, val ) store2(ofs, val)
+#define MINIRV32_STORE1( ofs, val ) store1(ofs, val)
+#define MINIRV32_LOAD4( ofs ) load4(ofs)
+#define MINIRV32_LOAD2( ofs ) load2(ofs)
+#define MINIRV32_LOAD1( ofs ) load1(ofs)
+
+#include "mini-rv32ima.h"
+
+uint8_t * ram_image = 0;
+struct MiniRV32IMAState * core;
+const char * kernel_command_line = 0;
